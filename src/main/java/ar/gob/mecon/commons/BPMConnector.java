d9eeba3b42b12d1f6ecd09ab8e689dec1dd56a12
Ruben Ghio <rubenghio@gmail.com>
2016-02-25 09:40:50 -0300

    Actualizacion de versiones para BPMS 6.2

--- src/main/java/ar/gob/mecon/commons/BPMConnector.java
+++ src/main/java/ar/gob/mecon/commons/BPMConnector.java
@@ -11,35 +11,34 @@ import org.kie.remote.client.api.RemoteRuntimeEngineFactory;
 import org.kie.remote.jaxb.gen.GetProcessIdsCommand;
 
 public class BPMConnector {
-	private static final String BASE_URL = "http://{HOST}:{PORT}/business-central/";
-	private KieSession remoteSession;
+    private static final String BASE_URL = "http://{HOST}:{PORT}/business-central/";
+    private KieSession remoteSession;
 
-	public BPMConnector(URL baseURL, String deploymentId, String user,
-			String password) {
-		RuntimeEngine engine = RemoteRuntimeEngineFactory.newRestBuilder()
-				.addUrl(baseURL).addTimeout(5).addDeploymentId(deploymentId)
-				.addUserName(user).addPassword(password).build();
-		remoteSession = engine.getKieSession();
-	}
+    public BPMConnector(URL baseURL, String deploymentId, String user,
+                        String password) {
+        RuntimeEngine engine = RemoteRuntimeEngineFactory.newRestBuilder()
+                .addUrl(baseURL).addTimeout(5).addDeploymentId(deploymentId)
+                .addUserName(user).addPassword(password).build();
+        remoteSession = engine.getKieSession();
+    }
 
-	@SuppressWarnings("unchecked")
-	public List<String> getProcessIds() throws MalformedURLException {
-		Command<List<String>> command = new GetProcessIdsCommand();
-		return remoteSession.execute(command);
-	}
+    @SuppressWarnings("unchecked")
+    public List<String> getProcessIds() throws MalformedURLException {
+        Command<List<String>> command = new GetProcessIdsCommand();
+        return remoteSession.execute(command);
+    }
 
-	public static void main(String[] args) throws Exception {
-		if (args.length != 5) {
-			throw new Exception(
-					"String 'host', String 'port', String 'deploymentId', String 'user', String 'password' son requeridos");
-		}
-		String baseURL = BASE_URL.replace("{HOST}", args[0]).replace("{PORT}",
-				args[1]);
-		BPMConnector connector = new BPMConnector(new URL(baseURL), args[2],
-				args[3], args[4]);
-		List<String> processes = connector.getProcessIds();
-		for (String process : processes) {
-			System.out.println("-----Proceso: " + process);
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        if (args.length != 5) {
+            throw new Exception(
+                    "String 'host', String 'port', String 'deploymentId', String 'user', String 'password' son requeridos");
+        }
+        String baseURL = BASE_URL.replace("{HOST}", args[0]).replace("{PORT}",
+                args[1]);
+        BPMConnector connector = new BPMConnector(new URL(baseURL), args[2], args[3], args[4]);
+        List<String> processes = connector.getProcessIds();
+        for (String process : processes) {
+            System.out.println("-----Proceso: " + process);
+        }
+    }
 }
